0441afaf32245fa1f46ec9d48d43ecfa05039de6
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2024-08-12 16:38:51 +0600

    add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Log the user in after signing up
+      flash[:notice] = "Welcome to the Blog, #{@user.username}!"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
