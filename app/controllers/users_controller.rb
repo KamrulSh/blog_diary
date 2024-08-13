0ad47e24e831b523e58664870da75df03d8b2fa4
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2024-08-13 10:19:34 +0600

    users crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User updated successfully"
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,6 +25,18 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render "new"
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      flash[:alert] = "User not found"
+      redirect_to users_path
     end
   end
 
